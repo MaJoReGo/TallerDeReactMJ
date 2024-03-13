8b6dfbd4c9ee6b420b1201e726e30b382c4029d7
Maria Jose Restrepo <restrepogomezmariajose@gmail.com>
2024-03-12 23:04:55 -0500

    Creando la pagina

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import { CreateTask } from './components/CreateTask/CreateTask'
+
+
+function App() {
+  return (
+    <>
+    <Home />
+    </> 
+  )
+}
+
+export default App
